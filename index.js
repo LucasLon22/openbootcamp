1a6539f180d98a98ae0f8320d71fb3e6a7b4be79
LucasLon22 <crlucaslondero@gmail.com>
2023-03-22 19:20:20 -0300

    lista

---
+++ index.js
@@ -0,0 +1,12 @@
+const lista = [
+  "Lucas",
+  30,
+  true,
+  27 - 04 - 1992,
+  (libro = {
+    nombre: "padre RIco Padre Pobre",
+    Autor: "Robert Kisosaki",
+    fecha: "2000",
+    URL: "https://www.audible.com/pd/Padre-Rico-Padre-Pobre-Rich-Dad-Poor-Dad-Audiobook/B01H4E8UP6?source_code=UHSPP30DTRIAL5471013220181&&cvosrc=ppc%20content.googlegdn.18597070948&cvo_crid=628396496159&cvo_pid=142410724077&source_code=&gclid=CjwKCAjwzuqgBhAcEiwAdj5dRh0-3443cuDo9CmzHrtUv6DG04h54WmpWv2rdFiICoi_BGB6D8EAVBoCUBMQAvD_BwE&gclsrc=aw.ds",
+  }),
+];
